1d4b338292b2c1364cce5852bdd1f68f5119de89
maalroba22 <maalroba22@gmail.com>
2023-01-30 14:55:55 -0500

    add to preprosesador sass and bootsrap

--- src/components/ListPost.vue
+++ src/components/ListPost.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <h1>listado de Post</h1>
+        <h1 class="btn btn-danger">listado de Post</h1>
         <ul class="list_post">
             <li v-for="post in Posts " :key="post.id">{{ post.title }} </li>
         </ul>
@@ -21,17 +21,22 @@ onMounted(async () => {
 
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 .list_post {
     width: 95%;
     height: 75px;
     padding: 20px;
     list-style-type: none;
-}
 
-.list_post li {
-    width: 100%;
-    padding: 10px;
-    border: 1px solid #ccc
+    li {
+        width: 100%;
+        padding: 10px;
+        border: 1px solid #ccc;
+        color: $red;
+    }
+    li:hover{
+        background-color: darken( #000000, 10%);
+    }
+   
 }
 </style>
